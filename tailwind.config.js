0eb96149a50543229dfa089c0bdda3a315a55e3d
Matheus <matheusgcm10@outlook.com>
2024-05-15 22:21:06 -0300

    feat: initial settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+      colors: {
+        'yellow-b2': '#FDCF00',
+        'blue-b2': '#02274f',
+        'red-b2': '#F62323',
+        'black-2': '#262626',
+        'gray-6': '#999999',
+        'white-1': '#FAFAFA',
+        'white-2': '#F1F5F9',
+        'white-3': '#F1F1F1',
+      },
+    },
   },
   plugins: [],
 }
